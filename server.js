63a95fd6a7b44b28c646c4d4abbdbd9a7bc0b366
aalokb <borkaraa@oregonstate.edu>
2018-05-29 21:47:18 -0700

    Added Node.js serving stack (templating, views, partials), 404 page

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
+
 	var path = require('path');
 	var express = require('express');
+	var exphbs = require('express-handlebars');
 	var fs = require('fs');
 	var app = express();
 
+	app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+	app.set('view engine', 'handlebars');
+
 	var port = process.env.PORT || 3000;
-	
+	var info = [];
+	app.get('/', function (req, res) {
+	  res.status(200).render('index', {data: info}); //index: should you render create twit button 
+	});
+
 	app.use(express.static('public'));
 
-	app.get('*', function (req, res) { //if its anything else than a specified URL (*)...
-	  res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+	app.get('*', function (req, res) {
+	  res.status(404).render('error');
 	});
 
 	app.listen(port, function () {
